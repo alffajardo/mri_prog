601a74694869362d5e078a02250bf9b2ad5138c6
Omar Narvaez <om.narv@gmail.com>
2018-12-13 17:36:29 -0600

    Add files via upload
    Con todos los scripts

--- ants.sh
+++ ants.sh
@@ -1,8 +1,24 @@
-#De neurodebian http://neuro.debian.net/install_pkg.html?p=ants fue copiado el siguiente codigo seleccionando la opción "only software with..."
+___
+sudo apt-get -y install build-essential
+sudo apt-get -y install git
+sudo apt-get -y install cmake
+sudo apt-get -y install cmake-curses-gui
+sudo apt-get -y install zlib1g-dev
+__
+mkdir ~/code 
+cd ~/code
+git clone https://github.com/stnava/ANTs.git
+__
+mkdir -p ~/bin/ants
+cd ~/bin/ants
+ccmake ~/code/ANTs
+__
+git config --global url."https://".insteadOf git://
+__
+make
+__
+If you want to use ANTs scripts, copy them from the source directory Scripts/ to the bin directory where antsRegistration etc are located.
+#Agregar a bashrc
+export ANTSPATH=${HOME}/bin/ants/bin
+export PATH=${ANTSPATH}:$PATH
 
-wget -O- http://neuro.debian.net/lists/bionic.us-ca.libre | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
-#Una vez terminado, colocar lo siguiente 
-sudo apt-get update
-#SI todo fue en orden, por ultimo colocar 
-sudo apt-get install ants
